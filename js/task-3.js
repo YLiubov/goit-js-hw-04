d383ca1f9dda60b4f9b2f8894342daeb9a60cfb9
YLiubov <129996599+YLiubov@users.noreply.github.com>
2024-02-01 21:49:44 +0100

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,22 @@
-"use strict";
-
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    
-    const boxSizing = "border-box";
-    let totalWidth;
-    if (boxSizing === "border-box") {
-    
-    totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-  } else {
-    totalWidth = contentWidth + 2 * paddingWidth;
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername: function(newName) {
+    this.username = newName;
+  },
+  updatePlayTime: function(hours) {
+    this.playTime += hours;
+  },
+  getInfo: function() {
+    return this.username + " has " + this.playTime + " active hours!";
   }
-  return totalWidth;
-}
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px"));
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
